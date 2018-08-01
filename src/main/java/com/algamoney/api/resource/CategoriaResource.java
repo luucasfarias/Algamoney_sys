fefc025aeeb4ecd85f30bf9739799dda4b31581a
lucas <luucasfarias21@gmail.com>
2018-07-31 22:10:46 -0300

    Controller de categoria

--- src/main/java/com/algamoney/api/resource/CategoriaResource.java
+++ src/main/java/com/algamoney/api/resource/CategoriaResource.java
@@ -1,11 +1,20 @@
 package com.algamoney.api.resource;
 
+import java.net.URI;
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.algamoney.api.model.Categoria;
 import com.algamoney.api.repository.CategoriaRepository;
@@ -15,9 +24,30 @@ import com.algamoney.api.repository.CategoriaRepository;
 public class CategoriaResource {
 	@Autowired
 	private CategoriaRepository categoriaRepository;
-	
+
 	@GetMapping
 	public List<Categoria> listar() {
 		return categoriaRepository.findAll();
+	}
+
+	@PostMapping
+	public ResponseEntity<Categoria> criar(@Valid @RequestBody Categoria categoria, HttpServletResponse response) {
+		Categoria categoriaSalva = categoriaRepository.save(categoria);
+		// Importante para o retorno do header na resposta da requisiçao.
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}")
+				.buildAndExpand(categoriaSalva.getId()).toUri();
+		response.setHeader("Location", uri.toASCIIString());
+		return ResponseEntity.created(uri).body(categoriaSalva);
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Categoria> buscarId(@PathVariable Long id) {
+		Categoria categoria = categoriaRepository.findOne(id);
+		// checa se a categoria(id) é um valor que exista cadastrado.
+		if (categoria != null) {
+			return ResponseEntity.ok(categoria);
+		} else {
+			return ResponseEntity.notFound().build();
+		}
 	}
 }
