a9a5a6895b3c7ece554f84e251b532aa84b80166
lucas farias <lucas.farias@localhost.localdomain>
2018-09-10 20:17:17 -0300

    version falid in authorization local

--- src/main/java/com/algamoney/api/config/AuthorizationServerConfig.java
+++ src/main/java/com/algamoney/api/config/AuthorizationServerConfig.java
@@ -1,21 +1,28 @@
 package com.algamoney.api.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenEnhancer;
+import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
+import com.algamoney.api.config.token.CustomTokenEnhancer;
+
+@Profile("oauth-security")
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
-
 	@Autowired
 	private AuthenticationManager authenticationManager;
 
@@ -23,18 +30,17 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
 		clients.inMemory().withClient("angular").secret("@ngul@r0").scopes("read", "write")
 				.authorizedGrantTypes("password", "refresh_token").accessTokenValiditySeconds(1800)
-				.refreshTokenValiditySeconds(3600 * 24)
-				.and()
-				.withClient("mobile").secret("m0b1l30").scopes("read")
+				.refreshTokenValiditySeconds(3600 * 24).and().withClient("mobile").secret("m0b1l30").scopes("read")
 				.authorizedGrantTypes("password", "refresh_token").accessTokenValiditySeconds(1800)
 				.refreshTokenValiditySeconds(3600 * 24);
 	}
 
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-		endpoints.tokenStore(tokenStore())
-				.accessTokenConverter(accessTokenConverter())
-				.reuseRefreshTokens(false)
+		TokenEnhancerChain tokenEnhancerChain = new TokenEnhancerChain();
+		tokenEnhancerChain.setTokenEnhancers(Arrays.asList(tokenEnhancer(), accessTokenConverter()));
+
+		endpoints.tokenStore(tokenStore()).tokenEnhancer(tokenEnhancerChain).reuseRefreshTokens(false)
 				.authenticationManager(authenticationManager);
 	}
 
@@ -48,5 +54,10 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	@Bean
 	public TokenStore tokenStore() {
 		return new JwtTokenStore(accessTokenConverter());
+	}
+
+	@Bean
+	public TokenEnhancer tokenEnhancer() {
+		return new CustomTokenEnhancer();
 	}
 }
--- src/main/java/com/algamoney/api/model/Lancamento_.java
+++ src/main/java/com/algamoney/api/model/Lancamento_.java
@@ -19,7 +19,6 @@ public abstract class Lancamento_ {
 	public static volatile SingularAttribute<Lancamento, LocalDate> dataPagemento;
 	public static volatile SingularAttribute<Lancamento, Long> id;
 	public static volatile SingularAttribute<Lancamento, String> descricao;
-	public static SingularAttribute dataPagamento;
 
 }
 
